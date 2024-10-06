bf652d659d63651ba565ddf79f610b4e9fdaaf8d
Jeremiah Lena <jeremiahlena13@gmail.com>
2024-10-06 23:22:02 +0100

    initialized repo

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello there</h1>
+      <p>How are you doing?</p>
+      <Input />
+    </div>
+  );
+}
